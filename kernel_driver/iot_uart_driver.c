cf9e5f8ba765b47d0097cead4c2254b8259977c2
vishwajit-tiwari <vishwajit16897@gmail.com>
2026-05-23 16:49:24 +0530

    Add buffered Linux UART character driver with kfifo, poll/select, wait queues, and UART RX framework

--- kernel_driver/iot_uart_driver.c
+++ kernel_driver/iot_uart_driver.c
@@ -19,8 +19,17 @@
 #include <linux/cdev.h>
 #include <linux/device.h>
 #include <linux/uaccess.h>
+
 #include <linux/kfifo.h>
 #include <linux/mutex.h>
+#include <linux/wait.h>
+#include <linux/poll.h>
+#include <linux/sched.h>
+
+#include <linux/kthread.h>
+#include <linux/delay.h>
+#include <linux/file.h>
+#include <linux/fcntl.h>
 
 
 /// Device information
@@ -30,16 +39,33 @@
 // FIFO buffer size for UART communication
 #define FIFO_SIZE 1024
 
+// UART device file path (this can be adjusted based on the actual hardware setup)
+#define UART_DEVICE "/dev/ttyAMA0"
+#define UART_BUFFER_SIZE 128
+
 /// Global variables for device management
 static dev_t dev_num;
 static struct cdev iot_cdev;
 static struct class *iot_class;
+
+// Thread and file pointer for UART communication
+static struct task_struct *uart_thread;
+static struct file *uart_filp;
+
+// Function prototype for the UART receive thread
+static int uart_rx_thread(void *data);
 
 // FIFO buffer for UART communication
 static DECLARE_KFIFO(iot_fifo, char, FIFO_SIZE);
 
 // Mutex for synchronizing access to the FIFO buffer
 static DEFINE_MUTEX(iot_mutex);
+
+// Wait queue for handling asynchronous I/O
+static DECLARE_WAIT_QUEUE_HEAD(iot_wait_queue);
+
+// Poll function prototype for handling asynchronous I/O
+static __poll_t iot_poll(struct file *file, poll_table *wait);
 
 // File operations prototypes
 static int iot_open(struct inode *inode, struct file *file);
@@ -54,6 +80,7 @@ static struct file_operations fops = {
     .release = iot_release,
     .read    = iot_read,
     .write   = iot_write,
+    .poll    = iot_poll,
 };
 
 // open function - called when the device is opened
@@ -78,14 +105,22 @@ static ssize_t iot_read(struct file *file, char __user *buf, size_t len, loff_t 
     char kbuf[128];
     int copied;
 
-    mutex_lock(&iot_mutex);
-
-    if (kfifo_is_empty(&iot_fifo)) 
+    // Non-blocking mode support
+    if (file->f_flags & O_NONBLOCK) 
     {
-        mutex_unlock(&iot_mutex);
-        pr_info("iot_uart: FIFO empty\n");
-        return 0;
+        if (kfifo_is_empty(&iot_fifo)) 
+        {
+            pr_info("iot_uart: FIFO empty, non-blocking read\n");
+            return -EAGAIN;
+        }
+    } 
+    else 
+    {
+        // Blocking mode - wait until data is available
+        wait_event_interruptible(iot_wait_queue, !kfifo_is_empty(&iot_fifo));
     }
+
+    mutex_lock(&iot_mutex);
 
     copied = kfifo_out(&iot_fifo, kbuf, min((unsigned int)len, kfifo_len(&iot_fifo)));
 
@@ -131,11 +166,79 @@ static ssize_t iot_write(struct file *file, const char __user *buf, size_t len, 
 
     kfifo_in(&iot_fifo, kbuf, len);
 
+    // Wake up any processes waiting for data to be available in the FIFO
+    wake_up_interruptible(&iot_wait_queue);
+
     mutex_unlock(&iot_mutex);
 
     pr_info("iot_uart: wrote %zu bytes into FIFO\n", len);
 
     return len;
+}
+
+// poll function - called to check the status of the device for asynchronous I/O
+static __poll_t iot_poll(struct file *file, poll_table *wait)
+{
+    __poll_t mask = 0;
+
+    poll_wait(file, &iot_wait_queue, wait);
+
+    if (!kfifo_is_empty(&iot_fifo))
+    {
+        mask |= POLLIN | POLLRDNORM;
+    }
+
+    return mask;
+}
+
+// UART receive thread function - continuously reads data from the UART device and stores it in the FIFO buffer
+static int uart_rx_thread(void *data)
+{
+    char rx_buf[UART_BUFFER_SIZE];
+    loff_t pos = 0;
+    int bytes_read;
+
+    pr_info("iot_uart: UART RX thread started\n");
+
+    // Open ttyAMA0 for reading
+    uart_filp = filp_open(UART_DEVICE, O_RDONLY, 0);
+
+    if (IS_ERR(uart_filp))
+    {
+        pr_err("iot_uart: failed to open %s\n", UART_DEVICE);
+
+        return PTR_ERR(uart_filp);
+    }
+
+    pr_info("iot_uart: opened %s\n", UART_DEVICE);
+
+    while (!kthread_should_stop())
+    {
+        memset(rx_buf, 0, sizeof(rx_buf));
+
+        bytes_read = kernel_read(uart_filp, rx_buf, sizeof(rx_buf) - 1, &pos);
+
+        if (bytes_read > 0)
+        {
+            mutex_lock(&iot_mutex);
+
+            kfifo_in(&iot_fifo, rx_buf, bytes_read);
+
+            mutex_unlock(&iot_mutex);
+
+            wake_up_interruptible(&iot_wait_queue);
+
+            pr_info("iot_uart: RX %d bytes: %s\n", bytes_read, rx_buf);
+        }
+
+        msleep(100);
+    }
+
+    filp_close(uart_filp, NULL);
+
+    pr_info("iot_uart: UART RX thread stopped\n");
+
+    return 0;
 }
 
 // Module initialization function - called when the module is loaded
@@ -198,12 +301,33 @@ static int __init iot_driver_init(void)
 
     pr_info("iot_uart: buffered driver loaded\n");
 
+    // Start the UART receive thread
+    uart_thread = kthread_run(uart_rx_thread, NULL, "iot_uart_rx");
+
+    if (IS_ERR(uart_thread))
+    {
+       pr_err("iot_uart: failed to create UART thread\n");
+
+       device_destroy(iot_class, dev_num);
+       class_destroy(iot_class);
+       cdev_del(&iot_cdev);
+       unregister_chrdev_region(dev_num, 1);
+
+       return PTR_ERR(uart_thread);
+    }
+
     return 0;
 }
 
 // Module exit function - called when the module is unloaded
 static void __exit iot_driver_exit(void)
 {
+    // Stop the UART receive thread
+    if (uart_thread)
+    {
+       kthread_stop(uart_thread);
+    }
+
     device_destroy(iot_class, dev_num); // Remove the device node
     class_destroy(iot_class); // Destroy the device class
     cdev_del(&iot_cdev); // Remove the character device from the kernel
